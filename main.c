8c95b208aeae7e502968a7c665beedc9a7b3663e
Gaetano <gaetanox109@gmail.com>
2021-12-20 22:32:03 -0500

    Modified onusercreate so it only initializes vars

--- main.c
+++ main.c
@@ -13,27 +13,16 @@
 #define cls()               SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); \
                                                     SDL_RenderClear(renderer) 
 
-void on_user_create(SDL_Window *window, SDL_Renderer *renderer, int close_requested);
-
-
-int main(int argc, char **argv) {
+void on_user_create(SDL_Window *window, SDL_Renderer *renderer)
 	SDL_Init(SDL_INIT_EVERYTHING);
 
     int close_requested;
 	SDL_Window *window;
 	SDL_Renderer *renderer;
-
-    /* Game Loop */
-    on_user_create(window, renderer, close_requested);
-
-    return 0;
-}
-
-void on_user_create(SDL_Window *window, SDL_Renderer *renderer, int close_requested) {
-    window = SDL_CreateWindow("Rasterizer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_Event e;
 
+    /* Game Loop */
+    on_user_create(window, renderer);
 
     while (!close_requested) {
         SDL_PollEvent(&e);
@@ -45,10 +34,17 @@ void on_user_create(SDL_Window *window, SDL_Renderer *renderer, int close_reques
             cls(); // clear screen
             /*  Draw  Here  */
 
-            
+
         
             /* Stop Drawing */
             SDL_RenderPresent(renderer);
         }
     }
+
+    return 0;
+}
+
+void on_user_create(SDL_Window *window, SDL_Renderer *renderer) {
+    window = SDL_CreateWindow("Rasterizer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, 0);
 }
