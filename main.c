5729ebf5e0e5f14814caffb443d4e2fe53595d58
Gaetano <gaetanox109@gmail.com>
2021-12-22 20:47:48 -0500

    Added a function for multiplying vectors and matrices

--- main.c
+++ main.c
@@ -108,3 +108,17 @@ void projection_matrix(Mat4x4 *mat_proj, float near, float far, float fov) {
     mat_proj->m[2][3] = 1;
     mat_proj->m[3][3] = 0;
 }
+
+/* Multiply a vector and a matrix */
+void mmv(Vec3D *o, Vec3D i, Mat4x4 m) {
+    o->x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+	o->y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+	o->z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+	float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+	if (w != 0.0f) {
+		o->x /= w; 
+        o->y /= w; 
+        o->z /= w;
+    }
+}
