bf400ff3f15aaf5c97f33f73b6cdb1522aa09dc5
Gaetano <gaetanox109@gmail.com>
2021-12-22 20:18:00 -0500

    After much debugging, I initialized the mesh.

--- main.c
+++ main.c
@@ -13,16 +13,19 @@
 #define cls()               SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); \
                                                     SDL_RenderClear(renderer) 
 
-void on_user_create(SDL_Window *window, SDL_Renderer *renderer)
+void on_user_create(SDL_Window *window, SDL_Renderer *renderer, Mesh *mesh);
+
+int main(int argc, char **argv) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
     int close_requested;
+    Mesh mesh;
 	SDL_Window *window;
 	SDL_Renderer *renderer;
     SDL_Event e;
 
     /* Game Loop */
-    on_user_create(window, renderer);
+    on_user_create(window, renderer, &mesh);
 
     while (!close_requested) {
         SDL_PollEvent(&e);
@@ -44,7 +47,38 @@ void on_user_create(SDL_Window *window, SDL_Renderer *renderer)
     return 0;
 }
 
-void on_user_create(SDL_Window *window, SDL_Renderer *renderer) {
+void on_user_create(SDL_Window *window, SDL_Renderer *renderer, Mesh *mesh) {
     window = SDL_CreateWindow("Rasterizer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, 0);
+    mesh = malloc(sizeof(Mesh) * 12);
+    mesh->tris = malloc(12 * sizeof(Triangle));
+    
+    Triangle tris[12] = {
+
+		// SOUTH
+		(Triangle) { 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+		(Triangle) { 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+		// EAST                                                      
+		(Triangle) { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+		(Triangle) { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+		// NORTH                                                     
+		(Triangle) { 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+		(Triangle) { 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+		// WEST                                                      
+		(Triangle) { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+		(Triangle) { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+		// TOP                                                       
+		(Triangle) { 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+		(Triangle) { 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+		// BOTTOM                                                    
+		(Triangle) { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+		(Triangle) { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+	};
+
+    mesh->tris = tris;
 }
--- rmath.h
+++ rmath.h
@@ -1,7 +1,5 @@
 #ifndef R_MATH_H
     #define R_MATH_H
-    #define vec3(x, y, z)                               (Vec3D) { x, y, z };
-    #define tri(x0, y0, z0, x1, y1, z1, x2, y2, z2)     (Triangle) { {vec3(x0, y0, z0), vec3(x1, y1, z1), vec3(x2, y2, z2), } };
 
     typedef struct vec3d {
         float x, y, z;
