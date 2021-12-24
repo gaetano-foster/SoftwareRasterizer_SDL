19a7e4e5ec063ce7962e8e2bb88bf3d423d0d01b
Gaetano <gaetanox109@gmail.com>
2021-12-24 11:05:13 -0500

    Moved init_mat function to rmath.c.

--- rmath.h
+++ rmath.h
@@ -15,13 +15,6 @@
         float m[4][4];
     } Mat4x4;
 
-    void init_mat(Mat4x4 *m)
-    {
-        for (int r = 0; r < 4; r++) {
-            for (int c = 0; c < 4; c++) {
-                m->m[c][r] = ( r == c ? 1.0f : 0.0f);
-            }
-        }
-    }
+    void init_mat(Mat4x4 *m);
 
 #endif
