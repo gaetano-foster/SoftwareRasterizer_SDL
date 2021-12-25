4070acd5660ff7c879a36f787e822061c6bcf275
Gaetano <gaetanox109@gmail.com>
2021-12-24 22:55:28 -0500

    Moved functions to rmath.h and renamed it to engine.h

---
+++ engine.h
@@ -0,0 +1,27 @@
+#ifndef R_MATH_H
+    #define R_MATH_H
+
+    typedef struct vec3d {
+        float x, y, z;
+    } Vec3D;
+
+    typedef struct triangle {
+        Vec3D p[3];
+    } Triangle;
+
+    typedef Triangle* Mesh;
+
+    typedef struct mat4x4 {
+        float m[4][4];
+    } Mat4x4;
+
+    void init_mat(Mat4x4 *m);
+    void projection_matrix(Mat4x4 *mat_proj, float near, float far, float fov);
+    void mmv(Vec3D *o, Vec3D i, Mat4x4 m);
+
+    // draw
+    void draw_triangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3);
+    void q_fill_triangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3);
+    void fill_triangle(SDL_Renderer *renderer, int x0, int y0, int x1, int y1, int x2, int y2);
+
+#endif
--- main.c
+++ main.c
@@ -8,18 +8,13 @@
 #include <math.h>
 #include <time.h>
 #include <SDL.h>
-#include "rmath.h"
+#include "engine.h"
 
 #define SCREEN_WIDTH        1200
 #define SCREEN_HEIGHT       650
 #define ASPECT_RATIO        (float)SCREEN_HEIGHT / (float)SCREEN_WIDTH
 
 void on_user_create(SDL_Window *window, SDL_Renderer *renderer, Mesh mesh);
-void projection_matrix(Mat4x4 *mat_proj, float near, float far, float fov);
-void mmv(Vec3D *o, Vec3D i, Mat4x4 m);
-void draw_triangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3);
-void q_fill_triangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3);
-void fill_triangle(SDL_Renderer *renderer, int x0, int y0, int x1, int y1, int x2, int y2);
 
 int main(int argc, char **argv) 
 {
@@ -233,110 +228,4 @@ void on_user_create(SDL_Window *window, SDL_Renderer *renderer, Mesh mesh)
     for (int i = 0; i < 12; i++) {
         mesh[i] = tris[i];
     }
-}
-
-void projection_matrix(Mat4x4 *mat_proj, float near, float far, float fov) 
-{
-    float fov_rad = 1.0f / tanf(fov * 0.5f / 180.0f * 3.14159f);
-
-    init_mat(mat_proj);
-    mat_proj->m[0][0] = ASPECT_RATIO * fov_rad;
-    mat_proj->m[1][1] = fov_rad;
-    mat_proj->m[2][2] = far / (far - near);
-    mat_proj->m[3][2] = (-far * near) / (far - near);
-    mat_proj->m[2][3] = 1.0f;
-    mat_proj->m[3][3] = 0.0f;
-}
-
-/* Multiply a vector and a matrix */
-void mmv(Vec3D *o, Vec3D i, Mat4x4 m) 
-{
-    o->x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
-	o->y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
-	o->z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
-	float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
-
-	if (w != 0.0f) {
-		o->x /= w; 
-        o->y /= w; 
-        o->z /= w;
-    }
-}
-
-void draw_triangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3) 
-{
-    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
-    SDL_RenderDrawLine(renderer, x2, y2, x3, y3);
-    SDL_RenderDrawLine(renderer, x3, y3, x1, y1);
-}
-
-void q_fill_triangle(SDL_Renderer *renderer, int x0, int y0, int x1, int y1, int x2, int y2)
-{
-    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
-
-		dx = x2 - x1; dy = y2 - y1;
-		dx1 = abs(dx); dy1 = abs(dy);
-		px = 2 * dy1 - dx1;	py = 2 * dx1 - dy1;
-	
-		if (dy1 <= dx1) {
-			if (dx >= 0) {
-				x = x1; y = y1; xe = x2;
-			}
-			else {
-				x = x2; y = y2; xe = x1;
-			}
-			SDL_RenderDrawLine(renderer, x0, y0, x, y);
-	
-			for (i = 0; x < xe; i++)
-			{
-				x = x + 1;
-				if (px < 0)
-					px = px + 2 * dy1;
-				else {
-					if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
-						y = y + 1;
-					else
-						y = y - 1;
-					px = px + 2 * (dy1 - dx1);
-				}
-				SDL_RenderDrawLine(renderer, x0, y0, x, y);
-			}
-		}
-		else {
-			if (dy >= 0) {
-				x = x1; y = y1; ye = y2;
-			}
-			else {
-				x = x2; y = y2; ye = y1;
-			}
-	
-			SDL_RenderDrawLine(renderer, x0, y0, x, y);
-	
-			for (i = 0; y < ye; i++) {
-				y = y + 1;
-				if (py <= 0)
-					py = py + 2 * dx1;
-				else {
-					if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
-						x = x + 1;
-					else
-						x = x - 1;
-					py = py + 2 * (dx1 - dy1);
-				}
-				SDL_RenderDrawLine(renderer, x0, y0, x, y);
-			}
-		}
-}
-
-void fill_triangle(SDL_Renderer *renderer, int x0, int y0, int x1, int y1, int x2, int y2) 
-{
-    q_fill_triangle(renderer, x0, y0,
-                              x1, y1, 
-                              x2, y2);
-    q_fill_triangle(renderer, x2, y2,
-                              x0, y0, 
-                              x1, y1);
-    q_fill_triangle(renderer, x1, y1,
-                              x2, y2, 
-                              x0, y0);
 }
